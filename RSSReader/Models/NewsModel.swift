1909da7f1678e141ad3abf88333db0ec77b7d449
Максим Шурумов <Shurumov@MacBook-Air-Valeria.local>
2020-04-06 05:33:04 +0700

    Reddit parsing

--- RSSReader/Models/NewsModel.swift
+++ RSSReader/Models/NewsModel.swift
@@ -1,39 +1,70 @@
 import SwiftyXMLParser
 import SwiftSoup
 
+enum NewsSource: String {
+    case habr = "Хабр"
+    case reddit = "Reddit"
+}
+
 class NewsModel {
     
     let title: String
     var description: String = ""
     var imageUrl: String = ""
-    let source: String
+    let source: NewsSource
     let date: Date?
     var descriptionIsHidden: Bool = true
 
-    init(xml: XML.Accessor) {
-        title = xml.title.text ?? ""
+    init(newsSource: NewsSource, xml: XML.Accessor) {
         
-        if let descriptionXml = xml["description"].text,
-            let descriptionDocument: Document = try? SwiftSoup.parse(descriptionXml) {
-            if let elements = try? descriptionDocument.select("img").array(),
-                let firstElement = elements.first,
-                let url = try? firstElement.attr("src") {
-                imageUrl = url
-            }
+        switch newsSource {
+        case .habr:
+            title = xml.title.text ?? ""
+            
+            if let descriptionXml = xml["description"].text,
+                let descriptionDocument: Document = try? SwiftSoup.parse(descriptionXml) {
+                if let elements = try? descriptionDocument.select("img").array(),
+                    let firstElement = elements.first,
+                    let url = try? firstElement.attr("src") {
+                    imageUrl = url
+                }
 
-            if let text = try? descriptionDocument.text() {
-                description = text.withoutReadMore
+                if let text = try? descriptionDocument.text() {
+                    description = text.withoutReadMore
+                }
             }
+            
+            source = newsSource
+            
+            let dateString = xml.pubDate.text ?? ""
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+            dateFormatter.dateFormat = "E, d MMM yyyy HH:mm:ss Z"
+            let date = dateFormatter.date(from: dateString)
+            self.date = date
+            
+        case .reddit:
+            title = xml.title.text ?? ""
+            
+            if let descriptionXml = xml.content.text,
+                let descriptionDocument: Document = try? SwiftSoup.parse(descriptionXml) {
+                if let elements = try? descriptionDocument.select("img").array(),
+                    let firstElement = elements.first,
+                    let url = try? firstElement.attr("src") {
+                    imageUrl = url
+                }
+            }
+            
+            source = newsSource
+            
+            let dateString = xml.updated.text ?? ""
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+            let date = dateFormatter.date(from: dateString)
+            self.date = date
         }
         
-        source = xml.link.text ?? ""
-        
-        let dateString = xml.pubDate.text ?? ""
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX") // set locale to reliable US_POSIX
-        dateFormatter.dateFormat = "E, d MMM yyyy HH:mm:ss Z"
-        let date = dateFormatter.date(from: dateString)
-        self.date = date
     }
     
     
--- RSSReader/Services/MainService.swift
+++ RSSReader/Services/MainService.swift
@@ -4,14 +4,44 @@ import SwiftyXMLParser
 
 class MainService {
     
-    func getHabrXML(completion: @escaping ([NewsModel]) -> ()) {
+    func getNews(completion: @escaping ([NewsModel]) -> ()) {
+        var allNews = [NewsModel]()
+        
+        getHabrNews() { (habrNews) in
+            allNews += habrNews
+            
+            self.getRedditNews() { (redditNews) in
+                allNews += redditNews
+                allNews = allNews.sorted(by: { $0.date!.compare($1.date!) == .orderedDescending})
+                completion(allNews)
+            }
+        }
+    }
+    
+    func getHabrNews(completion: @escaping ([NewsModel]) -> ()) {
         Alamofire.request("https://habr.com/rss/hubs/all").responseData { response in
             if let data = response.data {
                 let xml = XML.parse(data)
                 
                 var newsItems = [NewsModel]()
                 for item in xml.rss.channel.item {
-                    let news = NewsModel(xml: item)
+                    let news = NewsModel(newsSource: .habr, xml: item)
+                    newsItems.append(news)
+                }
+                
+                completion(newsItems)
+            }
+        }
+    }
+    
+    func getRedditNews(completion: @escaping ([NewsModel]) -> ()) {
+        Alamofire.request("https://www.reddit.com/.rss").responseData { response in
+            if let data = response.data {
+                let xml = XML.parse(data)
+                
+                var newsItems = [NewsModel]()
+                for item in xml.feed.entry {
+                    let news = NewsModel(newsSource: .reddit, xml: item)
                     newsItems.append(news)
                 }
                 
