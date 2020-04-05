e75ac8953e263dd1b60ff42a9b97895b71cf4008
ShurumovMaxim <work6921@yandex.ru>
2020-04-06 04:25:49 +0700

    Revert "Feature/habr parsing"

---
+++ RSSReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSReader
+//
+//  Created by Максим Шурумов on 05.04.2020.
+//  Copyright © 2020 Максим Шурумов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
